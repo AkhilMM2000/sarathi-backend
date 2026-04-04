bf19bdb817c8c6fecdb415356a1c6876a9a28847
Akhil <manojakhil36@gmail.com>
2026-04-04 21:16:40 +0530

    fix(file): update fileRoutes to pass next function to FileController

--- src/presentation/routes/fileRoutes.ts
+++ src/presentation/routes/fileRoutes.ts
@@ -1,13 +1,12 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { container } from "tsyringe";
 import { FileController } from "../controllers/FileController";
 
 const router = Router();
 const fileController = container.resolve(FileController);
 
-router.get("/signed-url", async (req: Request, res: Response) => {
-  await fileController.getSignedUrl(req, res);
+router.get("/signed-url", (req: Request, res: Response, next: NextFunction) => {
+  fileController.getSignedUrl(req, res, next);
 });
-
 
 export default router;
